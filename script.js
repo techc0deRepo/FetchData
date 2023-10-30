ccdfcab751dadc93558ad5a630aeece1a945a716
techc0deRepo <vladivladu93@gmail.com>
2023-10-30 21:40:16 +0100

    Objected

--- script.js
+++ script.js
@@ -1,4 +1,240 @@
-const url = 'https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp';
+function App() {
+
+    var app = {
+        obj: {},
+        sets: 
+            {
+                genre: new Set(),
+                platform: new Set()
+            }
+        ,
+        filters: {
+            name: "",
+            tags: new Set(),
+            platform: new Set(),
+            order: "",
+        },
+        
+        // GET REQUEST TO OBTAIN ALL THE DATA
+        initApp: (url, options) => {
+            try {
+                fetch(url, options)
+                  .then((response) => response.json())
+                  .then((data) => app.main(data))
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        // SAVE THE DATA
+        main: (data) => {
+            app.obj = data;
+            app.resetCardContainer();
+            app.addSets(data);
+            app.createCards(data);
+            app.createTags();
+        },
+
+        resetCardContainer: () => {
+            while (cnt.firstChild) {
+                cnt.firstChild.remove()
+            }
+        },
+
+        createCards: (data) => {
+
+            data.forEach(element => {
+                const card = {
+                    id: element.id,
+                    general_info: {
+                    genre: element.genre,
+                    platform: element.platform,
+                    developer: element.developer,
+                    publisher: element.publisher,
+                    },
+                    game_desc: {
+                    title: element.title,
+                    thumb: element.thumbnail,
+                    desc: element.short_description,
+                    release: element.release_date,
+                    url: element.game_url
+                    }
+                }
+
+                app.appendCard(app.createCard(card));
+
+            });
+        },
+
+        addSets: (data) => {
+            data.forEach(item =>{
+                app.sets.genre.add(item.genre);
+                app.sets.platform.add(item.platform);
+            })
+        },
+
+        appendCard: (card) => {
+            
+            // Append 
+            cnt.append(card.card_cnt);
+            card.card_cnt.append(card.image, card.game_desc);
+            card.game_desc.append(card.title, card.desc, card.release, card.cta);
+
+        },
+
+        createCard: (card) => {
+            
+            // Create Elements
+            let card_cnt = document.createElement("div");
+            let game_desc = document.createElement("div");
+            let image = new Image();
+            let title = document.createElement("h4");
+            let desc = document.createElement("p");
+            let release = document.createElement("span");
+            let cta = document.createElement("button");
+
+            // Add values to the elements created
+            image.src= card.game_desc.thumb;
+            title.innerHTML = card.game_desc.title;
+            desc.innerHTML = card.game_desc.desc;
+            release.innerHTML = card.game_desc.release;
+
+            // Button customize
+            cta.innerText = "Go to website";
+            cta.type = "button";
+            cta.classList.add("button");
+            cta.addEventListener("click", () => {
+                window.location.href=card.game_desc.url
+            });
+
+            // Set CSS class
+            card_cnt.setAttribute("class","card");
+            image.setAttribute("class","image");
+            game_desc.setAttribute("class","game-desc");
+
+            let cardCreated = {
+                card_cnt: card_cnt,
+                game_desc: game_desc,
+                image: image,
+                title: title,
+                desc: desc,
+                release: release,
+                cta: cta
+            }
+
+            return cardCreated;   
+        },
+
+        createTags: () => {
+
+            app.sets.genre = Array.from(app.sets.genre).sort();
+        
+            // Create Tags
+            app.sets.genre.forEach(item => {
+                let tag = document.createElement("small");
+                tag.innerHTML = item;
+                tag.classList.add("tag");
+                aside_cnt.append(tag);
+
+                tag.addEventListener("click", (e) => {
+                    
+                    let name = e.target.innerText;
+                    
+                    if (app.filters.tags.has(name)){
+                        app.filters.tags.delete(name);
+                    } else {
+                        app.filters.tags.add(name);
+                    }
+                    
+                    e.target.classList.toggle("clicked");
+                    
+                    app.sortObject(app.obj, app.filters);
+                });
+            });
+        },
+
+        sortObject: function(obj, filters) {
+
+            app.resetCardContainer();
+            let sortedObj;
+
+            if (filters.tags.size !== 0){
+                sortedObj = app.sortByTagName(obj);
+            } else {
+                sortedObj = obj;
+            }
+            
+            /*
+            sortedObj = app.sortByName(sortedObj);
+
+            switch (filters.order) {
+            case "price-asc":
+                oggettone.sort((a, b) => {
+                return parseFloat(a.rooms[0].room_rates.rate_total.amount) - parseFloat(b.rooms[0].room_rates.rate_total.amount);
+                });  
+                break;
+            case "price-desc":
+                oggettone.sort((a, b) => {
+                return parseFloat(b.rooms[0].room_rates.rate_total.amount) - parseFloat(a.rooms[0].room_rates.rate_total.amount);
+                });
+                break;
+            case "rating-asc":
+                oggettone.sort((a, b) => {
+                return parseFloat(a.rating) - parseFloat(b.rating);
+                });
+                break;
+            case "rating-desc":
+                oggettone.sort((a, b) => {
+                return parseFloat(b.rating) - parseFloat(a.rating);
+                });
+                break;
+            default:
+                console.log("error sorting");
+                break;
+            }*/
+
+            app.createCards(sortedObj);
+        },
+
+        sortByTagName: (obj) => {
+        
+            var array = [];
+            
+            obj.forEach(item => {
+                if (app.filters.tags.has(item.genre)){
+                    array.push(item);
+                }
+            });
+
+            return array;
+        },
+
+        sortByName: (obj) => {
+        
+            var array = [];
+            
+            obj.forEach(item => {
+                if (app.filters.name.has(item.genre)){
+                    array.push(item);
+                }
+            });
+
+            return array;
+        },
+
+        event_search: btn.addEventListener("click", (e) => {
+            console.log(e.target);
+        }),
+    };
+
+    return {
+        app : app
+    };
+};
+
+// CONSTANTS
+
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,88 +243,12 @@ const options = {
 	}
 };
 
-
 const btn = document.getElementById("btn");
 const cnt = document.getElementById("cnt");
-const genre = new Set();
-const platform = new Set();
+const aside_cnt = document.getElementById("aside-cnt");
 
-function saveFetch(data){
+// APP INIT 
 
-    while (cnt.firstChild) {
-        cnt.firstChild.remove()
-    }
-    console.log(data);
+let game = new App();
 
-    data.forEach(element => {
-        const obj = {
-            id: element.id,
-            general_info: {
-            genre: element.genre,
-            platform: element.platform,
-            developer: element.developer,
-            publisher: element.publisher,
-            },
-            game_desc: {
-            title: element.title,
-            thumb: element.thumbnail,
-            desc: element.short_description,
-            release: element.release_date,
-            url: element.game_url
-            }
-        }
-
-    // Add values to Sets    
-    genre.add(obj.general_info.genre);
-    platform.add(obj.general_info.platform);
-
-    // Create Elements
-    let card = document.createElement("div");
-    let general_info = document.createElement("div");
-    let game_desc = document.createElement("div");
-    let image = new Image();
-    let title = document.createElement("h3");
-    let desc = document.createElement("p");
-    let release = document.createElement("span");
-    let cta = document.createElement("button");
-
-    // Add values to the elements created
-    image.src= obj.game_desc.thumb;
-    title.innerHTML = obj.game_desc.title;
-    desc.innerHTML = obj.game_desc.desc;
-    release.innerHTML = obj.game_desc.release;
-
-    // Button customize
-    cta.innerText = "Go to website";
-    cta.type = "button";
-    cta.classList.add("button");
-    cta.addEventListener("click", () => {
-        window.location.href=obj.game_desc.url
-    });
-
-    // Set CSS class
-    card.setAttribute("class","card");
-    image.setAttribute("class","image");
-    game_desc.setAttribute("class","game-desc");
-
-    // Append 
-    cnt.append(card);
-    card.append(image, game_desc);
-    game_desc.append(title, desc, release, cta);
-
-    // Reapeat to all the objects!
-    });
-
-    console.log(genre);
-    console.log(platform);
-}
-
-btn.addEventListener("click", () => {
-    try {
-        fetch(url, options)
-          .then((response) => response.json())
-          .then((data) => saveFetch(data))
-    } catch (error) {
-        console.log(error);
-    }
-});
+game.app.initApp(url, options);
